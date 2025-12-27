c37d531aaabb495baad62048e449c4a04370f28c
Javier Morgaz Garcia <javier.morgaz@free-now.com>
2025-12-27 16:23:28 +0100

    fix(ci): add testIgnore to exclude Jest files from Playwright

--- tests/playwright.config.js
+++ tests/playwright.config.js
@@ -7,6 +7,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: './e2e',
     testMatch: '**/*.spec.js',
+    testIgnore: '**/*.test.js',
     /* Maximum time one test can run for. */
     timeout: 30 * 1000,
     expect: {
